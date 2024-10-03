f791ecc3497fd78872cefcc0deb4800d5d8be0a3
ifat <i>
2024-10-04 00:02:43 +0100

    first problem solved

---
+++ HelloPrinter.java
@@ -0,0 +1,30 @@
+/**
+ * Open a simple text editor, or open a folder and create a new Java file in
+ * VSCode, and
+ * type in the following program:
+ * public class HelloPrinter {
+ * public static void main(String[] args) {
+ * System.out.println("Hello, Paul");
+ * }
+ * }
+ * Save your work in a file called HelloPrinter.java. Pay attention to the case
+ * of letters in your program and in the name of the file. Compile your program
+ * in your IDE or
+ * from the console window. If you use a console window
+ * javac HelloPrinter.java
+ * What files are contained in the directory after you have compiled the
+ * program?
+ * Run your program by typing the following command (again, if you are using a
+ * console):
+ * java HelloPrinter
+ * What is the output of your program? What is contained in the .class file?
+ */
+
+
+
+ public class HelloPrinter {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello, Jamil!");
+    }
+ }
